caf88044e03ab6c8e36c44c502cd68c88355695e
ju5tu5 <justus@ju5tu5.nl>
2022-11-25 10:20:10 +0100

    First component

--- .netlify/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const file = '_app/immutable/components/error.svelte-f96441c7.js';
-export const imports = ["_app/immutable/components/error.svelte-f96441c7.js","_app/immutable/chunks/index-0c8341f8.js","_app/immutable/chunks/singletons-ba0802be.js"];
-export const stylesheets = [];
-export const fonts = [];
