199322f34f93d493793a0c3c81a23826846ce090
Cyd Stumpel <info@cydstumpel.nl>
2022-11-25 11:50:43 +0100

    wip

---
+++ src/lib/molecules/TextBlock.svelte
@@ -0,0 +1,33 @@
+<script>
+  import '../styles/molecules/_text-block.css'
+  import Button from '../atoms/Button.svelte'
+  export let content = {};
+</script>
+
+<section class="text-block {content.image ? 'has-image': ''}">
+  <div class="text-block-inner {content.border ? 'text-block-rounding--' + content.border : 'text-block-rounding--top-right'}">
+    <div class="corner"></div>
+    <div class="text-block-text background--inherit">
+      <h2 class="medium-heading">
+        {content.title}
+      </h2>
+      <p class="small-body">
+        {content.text}
+      </p>
+      {#if content.button}
+        <Button url={content.button.url} title={content.button.title} />
+      {/if}
+      {#if content.footnote}
+        <p class="xsmall-body">
+          {@html content.footnote}
+        </p>
+      {/if}
+    </div>
+  </div>
+  {#if content.image}
+    <div class="text-block-image">
+      <img src={content.image.src} alt="{content.image.alt}">
+      <p class="xsmall-body">{content.image.caption}</p>
+    </div>
+  {/if}
+</section>
