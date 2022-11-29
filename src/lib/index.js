2fb754a3bbf664a6d25bb5d27e4c4270bd05eef8
ju5tu5 <justus@ju5tu5.nl>
2022-11-29 11:54:47 +0100

    Added BrandingHvA to index.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,3 +3,4 @@ import '$lib/styles/_basics.css';
 // Reexport your entry components here
 export { default as PageHeader } from './molecules/PageHeader.svelte';
 export { default as TextBlock } from './molecules/TextBlock.svelte';
+export { default as BrandingHvA } from './molecules/BrandingHvA.svelte';
