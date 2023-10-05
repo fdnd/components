58431fca11fe15d9fe4aca75ff6248f0050cff6f
Justus Sturkenboom <justus@ju5tu5.nl>
2023-10-05 09:25:43 +0200

    Added block component

---
+++ src/lib/templates/Block.svelte
@@ -0,0 +1,14 @@
+<script>
+  // $$restProps.class makes it possible to always inject a class should we want to
+</script>
+
+<div class="{$$restProps.class || ''}">
+  <slot />
+</div>
+
+<style>
+  div {
+    container-type: inline-size;
+    padding: var(--component-padding);
+  }
+</style>
